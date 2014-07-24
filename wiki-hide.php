f0543c0eb36e6df93f07457eb4719ca887131be0
miroush <mirek.joha@gmail.com>
2014-07-24 15:59:24 +0200

    Create wiki-hide.php

---
+++ wiki-hide.php
@@ -0,0 +1,25 @@
+<?
+/* your definition here */
+$myKeyString = "..!";
+$username = "myUser";
+$password = "myPass";
+$errorMessage = "You have to enter proper credentials to see the content!"
+/* end of your definition */
+
+if (strpos( $_SERVER[REQUEST_URI],$myKeyString) !== false) {
+
+	if (isset($_SERVER['PHP_AUTH_USER']) 
+			&& isset($_SERVER['PHP_AUTH_PW']) 
+			&& $_SERVER['PHP_AUTH_USER'] == $username
+			&& $_SERVER['PHP_AUTH_PW'] == $password) 
+	{ 
+		// all fine
+		
+	} else { // wrong credentials
+	  header('HTTP/1.0 401 Unauthorized');
+	  header('WWW-Authenticate: Basic realm="Login"');
+	  echo $errorMessage;
+	  exit;
+	}
+}
+?>
